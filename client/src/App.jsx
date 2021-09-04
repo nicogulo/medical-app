73fab8bd8694b1164e66de8f7e353a7934a6f26b
nicogulo <nicogulobelas@gmail.com>
2021-09-05 01:53:01 +0700

    client-side Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,16 @@ import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import './App.css';
 
-import { ChanellListContainer, ChannelContainer } from './components';
+import { ChanellListContainer, ChannelContainer, Auth } from './components';
 
 const apiKey = 'dve66avmjbed';
 const client = StreamChat.getInstance(apiKey);
+const authToken = false;
 
 export default function App() {
+  if (!authToken) {
+    return <Auth />;
+  }
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
